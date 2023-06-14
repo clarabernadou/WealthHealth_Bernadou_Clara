ec2beabcd03d6719b24b39fc04df7e4c87a75cab
Clara Bernadou <clarabernadou56690@gmail.com>
2023-06-14 05:17:38 +0200

    Filter table columns (address bug to be resolved)

--- frontend/src/Components/Table.jsx
+++ frontend/src/Components/Table.jsx
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSort } from "@fortawesome/free-solid-svg-icons";
 
 export default function Table() {
   const [employeeDataList, setEmployeeDataList] = useState([]);
   const [search, setSearch] = useState("");
+  const [sortOrder, setSortOrder] = useState("asc");
 
   useEffect(() => {
     const data = JSON.parse(localStorage.getItem("employeeDataList") || "[]");
@@ -13,29 +16,46 @@ export default function Table() {
     setSearch(event.target.value);
   };
 
+  const handleSort = (column) => {
+    
+    const sortedArray = filteredData.slice().sort((a, b) => {
+      const valueA = a[column];
+      const valueB = b[column];
+
+      if (sortOrder === "asc") {
+        return valueA.localeCompare(valueB);
+      } else {
+        return valueB.localeCompare(valueA);
+      }
+    });
+
+    const newSortOrder = sortOrder === "asc" ? "desc" : "asc";
+    setSortOrder(newSortOrder);
+    setEmployeeDataList(sortedArray);
+  };
+
   const filteredData = employeeDataList.filter((employee) => {
     const firstName = employee.firstName.toLowerCase();
     const lastName = employee.lastName.toLowerCase();
-    const birthDate = employee.birthDate.toLowerCase();
-    const startDate = employee.startDate.toLowerCase();
+    const birthDate = employee.birthDate;
+    const startDate = employee.startDate;
     const street = employee.address.street.toLowerCase();
     const city = employee.address.city.toLowerCase();
     const state = employee.address.state.toLowerCase();
-    const zipCode = employee.address.zipCode.toLowerCase();
+    const zipCode = employee.address.zipCode;
 
     return (
-      firstName.includes(search.toLowerCase()) ||
+      firstName.startsWith(search.toLowerCase()) ||
       lastName.includes(search.toLowerCase()) ||
-      birthDate.includes(search.toLowerCase()) ||
-      startDate.includes(search.toLowerCase()) ||
+      birthDate.includes(search) ||
+      startDate.includes(search) ||
       street.includes(search.toLowerCase()) ||
       city.includes(search.toLowerCase()) ||
       state.includes(search.toLowerCase()) ||
-      zipCode.includes(search.toLowerCase())
+      zipCode.includes(search)
     );
   });
-
-  console.log(search);
+  console.log(filteredData);
 
   return (
     <div className="table-wrapper">
@@ -59,15 +79,60 @@ export default function Table() {
       <table className="table" id="table">
         <thead>
           <tr>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Start Date</th>
-            <th>Department</th>
-            <th>Date of Birth</th>
-            <th>Street</th>
-            <th>City</th>
-            <th>State</th>
-            <th>Zip Code</th>
+            <th className="table-option" onClick={() => handleSort("firstName")}>
+              <div className="sort-arrow">
+                First Name
+                <FontAwesomeIcon icon={faSort} />
+              </div>
+            </th>
+            <th className="table-option" onClick={() => handleSort("lastName")}>
+              <div className="sort-arrow">
+                Last Name
+                <FontAwesomeIcon icon={faSort} />
+              </div>
+            </th>
+            <th className="table-option" onClick={() => handleSort("startDate")}>
+              <div className="sort-arrow">
+                Start Date
+                <FontAwesomeIcon icon={faSort} />
+              </div>
+            </th>
+            <th className="table-option" onClick={() => handleSort("department")}>
+              <div className="sort-arrow">
+                Department
+                <FontAwesomeIcon icon={faSort} />
+              </div>
+            </th>
+            <th className="table-option" onClick={() => handleSort("birthDate")}>
+              <div className="sort-arrow">
+                Date of Birth
+                <FontAwesomeIcon icon={faSort} />
+              </div>
+            </th>
+            <th className="table-option" onClick={() => handleSort("address.street")}>
+              <div className="sort-arrow">
+                Street
+                <FontAwesomeIcon icon={faSort} />
+              </div>
+            </th>
+            <th className="table-option" onClick={() => handleSort("address.city")}>
+              <div className="sort-arrow">
+                City
+                <FontAwesomeIcon icon={faSort} />
+              </div>
+            </th>
+            <th className="table-option" onClick={() => handleSort("address.state")}>
+              <div className="sort-arrow">
+                State
+                <FontAwesomeIcon icon={faSort} />
+              </div>
+            </th>
+            <th className="table-option" onClick={() => handleSort("address.zipCode")}>
+              <div className="sort-arrow">
+                Zip Code
+                <FontAwesomeIcon icon={faSort} />
+              </div>
+            </th>
           </tr>
         </thead>
         <tbody className="custom-tbody">
